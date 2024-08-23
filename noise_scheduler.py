b45100d7a8a6a0bed6fe18faadd0cf13e9657fc0
Veeraraju-E <veerarajuel@gmail.com>
2024-08-23 19:27:46 +0530

    add_noise function completed

--- noise_scheduler.py
+++ noise_scheduler.py
@@ -32,9 +32,21 @@ class LinearNoiseScheduler:
         :param noise: the original noise sample
         :param t: which timestep? => decides exact amt of noise to add, based on derivation from original noise sample [B]
         one time step for every sample of the batch
-        :return:
+        :return: Image with noise added
         """
         original_shape = original.shape
         batch_size = original_shape[0]
 
+        # reshape to make final size => [B, 1, 1, 1]
         sqrt_alpha_cumulative = self.sqrt_alpha_cumulative[t].reshape(batch_size)
+        sqrt_one_minus_alpha_cumulative = self.sqrt_one_minus_alpha_cumulative[t].resize(batch_size)
+
+        for i in range(len(original_shape) - 1):
+            sqrt_alpha_cumulative = sqrt_alpha_cumulative.unsqueeze(-1)
+            sqrt_one_minus_alpha_cumulative = sqrt_one_minus_alpha_cumulative.unsqueeze(-1)
+            if i == 0:
+                print(f"sqrt_alpha_cumulative.shape : {sqrt_alpha_cumulative.shape}, sqrt_one_minus_alpha_cumulative.shape : {sqrt_one_minus_alpha_cumulative.shape}")
+
+        # we need to return sqrt(alpha_bar_t) * x0 + sqrt(1 - alpha_bar_t)*epsilon
+        print(f"noise.shape : {noise.shape}, original.shape : {original.shape}")
+        return sqrt_alpha_cumulative * original + sqrt_one_minus_alpha_cumulative * noise
