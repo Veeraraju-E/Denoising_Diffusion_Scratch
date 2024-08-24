cd4457f1231c0216278890b4b643fb69abd82ee0
Veeraraju-E <veerarajuel@gmail.com>
2024-08-24 23:07:00 +0530

    brief plan for model.py laid out

--- model.py
+++ model.py
@@ -3,7 +3,16 @@ import torch.nn as nn
 
 # Remember : Noisy Image -> Denoising Diffusion Distribution -> Probabilistic Model -> Predicted Noise
 # But, what about timestep information?
-# We need to use time embeddings
+# We need to use time embeddings (similar to transformer architecture)
+# time t -> pos.embedding -> FCN -> SILU -> FCN -> embedding
+
+# The UNet needs to take in time embedding information via self attention modules
+# A bit tricky to do from scratch for the first time, I'm borrowing implementation from https://github.com/explainingai-code/DDPM-Pytorch
+# We basically need a Downs block, a bottleneck and an Ups block
+# The Downs block needs to have a ResNet + Self Attention module, then a down-sampling layer
+# The bottleneck should also be modified - 1 ResNet, then Self Attention + ResNet module (HF implementation)
+# The Ups block would have the up-sampling layer, along with the same ResNet + Self Attention module (skip connections are transferred just like UNet)
+# Need to use the time-embeddings also (as part of input to ResNet?)
 
 class UNetModel(nn.Module):
     def __init__(self, in_channels=3, out_channels=3):
