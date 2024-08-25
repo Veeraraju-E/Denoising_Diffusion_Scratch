e8f6290ff157d44b82925ed664c9c40ab46cc304
Veeraraju-E <veerarajuel@gmail.com>
2024-08-25 16:05:23 +0530

    DownsBlock definition added

--- model.py
+++ model.py
@@ -6,13 +6,6 @@ import torch.nn as nn
 # We need to use time embeddings (similar to transformer architecture)
 # time t -> pos.embedding -> FCN -> SILU -> FCN -> embedding
 
-# The UNet needs to take in time embedding information via self attention modules
-# A bit tricky to do from scratch for the first time, I'm borrowing implementation from https://github.com/explainingai-code/DDPM-Pytorch
-# We basically need a Downs block, a bottleneck and an Ups block
-# The Downs block needs to have a ResNet + Self Attention module, then a down-sampling layer
-# The bottleneck should also be modified - 1 ResNet, then Self Attention + ResNet module (HF implementation)
-# The Ups block would have the up-sampling layer, along with the same ResNet + Self Attention module (skip connections are transferred just like UNet)
-# Need to use the time-embeddings also (where?!)
 
 # First, the method to get the time embeddings
 def get_time_embedding(time_steps, embedding_dim):
@@ -32,13 +25,54 @@ def get_time_embedding(time_steps, embedding_dim):
     )) / half_embedding_dim
     )
 
-    # evaluate only half of the embeddings, as we need to include both the sin and cosine portions, together which yields embedding_dim
+    # evaluate only half of the embeddings, as we need to include both the sin and cosine portions, which together yields embedding_dim
     half_time_embedding = time_steps[:None].repeat(1, half_embedding_dim) / common_factors
     time_embedding = torch.cat([torch.sin(half_time_embedding), torch.cos(half_embedding_dim)], dim=-1)
     return time_embedding
 
-class UNetModel(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3):
+# The UNet needs to take in time embedding information via self attention modules
+# A bit tricky to do from scratch for the first time, I'm borrowing implementation from https://github.com/explainingai-code/DDPM-Pytorch
+# We basically need a Downs block, a bottleneck and an Ups block
+# The Downs block needs to have a ResNet + Self Attention module, then a down-sampling layer
+# The bottleneck should also be modified - 1 ResNet, then Self Attention + ResNet module (HF implementation)
+# The Ups block would have the up-sampling layer, along with the same ResNet + Self Attention module (skip connections are transferred just like UNet)
+# Need to use the time-embeddings also (where?!)
+
+# Downs Block
+# x -> [Norm + SILU  + Conv] -> x_1
+# (x_1, cat with actual_time_embeddings) -> [Norm + SILU + Conv] -> x_2
+# (x_2, cat with x) for self attention -> [Norm + SA] -> x_3
+# (x_2, cat with x_3) -> Down Sample -> z
+
+class DownsBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, embedding_dim, down_sampler, num_attenion_heads):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.down_sampler = down_sampler
+
+        self.actual_time_embeddings_network = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(embedding_dim, out_channels)
+        )
+
+        self.resnet_block_conv_1 = nn.Sequential(
+            nn.GroupNorm(num_groups=8, num_channels=self.in_channels),
+            nn.SiLU(),
+            nn.Conv2d(self.in_channels, self.out_channels, 3, 1, 1)
+        )
+
+        self.resnet_block_conv_2 = nn.Sequential(
+            nn.GroupNorm(num_groups=8, num_channels=self.out_channels),
+            nn.SiLU(),
+            nn.Conv2d(self.out_channels, self.out_channels, 3, 1, 1)
+        )
+
+        self.attention_block_norm = nn.GroupNorm(num_groups=8, num_channels=self.out_channels)
+        self.attention_block_multihead = nn.MultiheadAttention(self.out_channels, num_attenion_heads, batch_first=True)
+
+        # a simple 1x1 Conv to make residual connection from the input to the output of the last conv layer
+        self.residual_connection = nn.Conv2d(self.in_channels, self.out_channels, 1)
+        # down sampling layer
+        self.down_sample = nn.Conv2d(self.out_channels, self.out_channels, kernel_size=4, stride=2, padding=1) \
+            if self.down_sampler else nn.Identity()
