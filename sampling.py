580b4f988c3330f137a41ee3c26ba64bcffcdaaa
Veeraraju-E <veerarajuel@gmail.com>
2024-12-06 23:26:22 +0530

    more nits

--- sampling.py
+++ sampling.py
@@ -47,7 +47,7 @@ def sample(
 
 def infer(args):
     #### Setup ####
-    with open(args.config_file, 'r') as file:
+    with open(args.config_path, 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as e:
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def train(args):
 
     #### Setup #### 
-    with open(args.config_file, 'r') as file:
+    with open(args.config_path, 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as e:
